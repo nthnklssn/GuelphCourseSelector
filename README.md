2aee8ce1d8bee251c295ef81b00d614e44e559d3
Nathan Klassen <nthnklssn@live.com>
2019-01-13 18:37:28 -0500

    Updated Readme, added enroll in course function as well as fixed a lot of issues and redundancies.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-Automated Course Enroller For University Of Guelph
+Automated Course Enroller For University Of Guelph.
+This code was made by Nathan Klassen, UoG
+Use this program at your own risk, as I am not responsible for any errors you make to your schedule.
+
+Usernames and Passwords you enter in the program are in no way stored or sent back to me.
+
+Requires Geckodriver, Selenium and python.
+
+***********
+USAGE
+***********
+This program will auto enroll you in up to 6 courses in a given semester.
+You must enter your WebAdvisor username and password and then the details of the courses you are taking,
+including section number.
+
+The program will continuosly try and enroll you in the courses.
+
+To run the program just type:
+
+    python webadvisorScript.py in the terminal
+
+Values such as username, passwords and course detailes can be hardcoded for speed/ testing.
+Uncomment line 49 in the web_utils file for the program to be functional otherwise it is just for demo purposes.
+***************
+Code BreakDown
+***************
+The code is seperated into two sections, web_utils contains the utilities used for the script.
+The code in web_utils could be re-used for more automation services on WebAdvisor.
+Code located in webadvisorScript is just used to automate and enroll in the desired courses.
+
+
+************
+Dependancies
+************
+
+Must have Geckodriver, Python 2.7, Selenium and Firefox installed on your computer.
--- web_utils.py
+++ web_utils.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 
@@ -12,19 +13,19 @@ class newDriver:
     def teardown(self):
         self.webdriver.close()
 
+    '''
+    Function to return to the course selection page.
+    '''
     def goBacktoSelect(self):
         self.webdriver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/ul/li[2]").click()
         self.webdriver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/ul[1]/li[2]").click()
-    def search(self, searchTerm):
-        elem = self.webdriver.find_element_by_xpath("//*[@id='lst-ib']")
-        elem.click()
-        elem.send_keys(searchTerm)
-        elem.send_keys(Keys.ENTER)
-        #print self.webdriver.find_elements_by_xpath("/html/body/div[6]/div[3]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div/div/h3/a")[0].text
 
+    '''
+    Logins to the WebAdvisor website using username and password you provide.
+    '''
     def login(self, username, password):
         self.webdriver.find_element_by_xpath("//*[@id='acctLogin']").click()
-        time.sleep(1)
+        time.sleep(3)
         elem = self.webdriver.find_element_by_xpath('//*[@id="USER_NAME"]')
         elem.click()
         elem.send_keys(username)
@@ -36,6 +37,29 @@ class newDriver:
         self.webdriver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[1]/ul/li[2]").click()
         self.webdriver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/ul[1]/li[2]").click()
 
+    '''
+    Attempts to enroll in the course described by the course dictionary passed inself.
+    Will return true if succesfully enrolled in the course
+    '''
+    def enrollInCourse(self, course):
+        try:
+            for i in range(30):
+                element = self.webdriver.find_element_by_id("SEC_SHORT_TITLE_" + str(i+1))
+
+                if course["code"] + "*" + course["section"] in element.get_attribute("innerHTML"):
+                    if self.webdriver.find_element_by_id("LIST_VAR2_" + str(i+1)).get_attribute("innerHTML").lower() == "open":
+                        print "Course not full, enrolled!"
+                        self.webdriver.find_element_by_id("LIST_VAR1_" + str(i+1)).click()
+                        #Uncomment the following line for functionality, I do not want to mess up courses while testing
+                        #self.webdriver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div[3]/form/div/input").click()
+                        return True
+                    else:
+                        print "Could not enroll in the course at the time, trying later"
+                        return False
+
+        except NoSuchElementException:
+            return False
+
     def enterTextValue(self, xpath, text):
         elem = self.webdriver.find_elements_by_xpath(xpath)[0]
         elem.send_keys(text)
@@ -46,9 +70,6 @@ class newDriver:
             for element in self.webdriver.find_elements_by_xpath(xpath):
                 if text in element.text:
                     element.click()
-
-    def findCheckElement(self, xpath, text):
-        pass
 
 
     def loadWebPage(self, webpageString):
